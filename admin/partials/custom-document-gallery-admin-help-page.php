b1f7265982c3bc75ba58672bdc56dd789c231812
Sonja Linton <sonja.linton@maxxpotential.com>
2018-07-21 08:11:52 -0400

    added class for table with galleries, made main page table of galleries, made started basic help page

---
+++ admin/partials/custom-document-gallery-admin-help-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Provide a help screen for the plugin.
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Custom_Document_Gallery
+ * @subpackage Custom_Document_Gallery/admin/partials
+ */
+
+$header_color = "DarkBlue";
+$strong_color = "CornflowerBlue";
+
+?>
+
+<div class="wrap">
+	<h2 style="color:<?php echo $heading_color; ?>;">Welcome to the Custom Document Gallery! Here is a brief tour of the plugin, how to use the backend and how to use the front-end.</h2>
+    <h3 style="color:<?php echo $heading_color; ?>;">How To Create/Edit Content</h3>
+
+    <h4>Create New Gallery</h4>
+	    <p>
+	        Create a new gallery by pressing the <strong style="color: <?php echo $strong_color; ?>;">Add New</strong> button at the top of the page.
+	    </p>
+    <h4>Edit Gallery</h4>
+
+</div>
--- admin/partials/custom-document-gallery-admin-main-page.php
+++ admin/partials/custom-document-gallery-admin-main-page.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Provide an admin area view for the plugin
+ * Provide an admin area view for the plugin.
  *
  * This file is used to markup the admin-facing aspects of the plugin.
  *
@@ -11,7 +11,29 @@
  * @package    Custom_Document_Gallery
  * @subpackage Custom_Document_Gallery/admin/partials
  */
+
+include( plugin_dir_path( __FILE__ ) . 'classes/class-custom-document-gallery-gallery-table.php');
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
-<h2> THIS IS THE TITLE </h2>
+<?php
+
+$table = new CDG_Gallery_Table();
+$table->prepare_items();
+?>
+<div class="wrap">
+
+    <div class="icon32 icon32-posts-post" id="icon-edit"><br></div>
+    <h2><?php _e('Galleries', 'mgpv_example')?>
+    	<a class="add-new-h2" href="<?php echo get_admin_url(get_current_blog_id(), 'admin.php?page=gallery-set-form');?>">
+    		<?php _e('Add new', 'mgpv_example')?>
+    	</a>
+    </h2>
+
+    <?php $table->views() ?>
+    <form id="gallery-set-filter" method="GET">
+        <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>"/>
+        <?php $table->display() ?>
+    </form>
+
+</div>
